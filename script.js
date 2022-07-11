ffe25d35a523c360e848692cc7e803200db500b3
Aankirz <sahuankit453@gmail.com>
2022-07-11 23:14:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ function deleteTask(taskId){}
 function addTask(task){}
 
 function showNotification(message){}
+
+class Person{
+    constructor(name){
+        this.name=name;
+        
+    }
+}
+var person=new Person('John');
+
+person.hasOwnProperty(property);
+
+class student extends Person{
+    constructor(name,className,rollNo){
+        super(name);
+        this.className=className;
+        this.rollNo=rollNo;
+
+    }
+     getDetails() {
+        var arr=[this.name,this.rollNo,this.className];
+      return arr;
+    }
+}
+var s1=new student('John','1st','1');
+
+class teacher extends Person{
+    constructor(name,teacherId,subject){
+        super(name);
+        this.teacherId=teacherId;
+        this.subject=subject;
+    }
+    getDetails(){
+        var arr=[this.name,this.teacherId,this.subject];
+        return arr;
+    }
+}
+
+
